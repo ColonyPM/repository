55a682c3abaa7a072dae8b8733f2da010b246cbd
vincent <61423159+Vincent-Gustafsson@users.noreply.github.com>
2025-11-21 13:16:02 +0100

    feat: package model and its admin page
    Co-authored-by: Lukaserikss0n <lukas.emil.eriksson@outlook.com>

--- packages/admin.py
+++ packages/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import DownloadEvent, Package
+
+
+@admin.register(Package)
+class PackageAdmin(admin.ModelAdmin):
+    # Columns in list view
+    list_display = (
+        "name",
+        "version",
+        "author",
+        "download_count",
+        "deprecated",
+        "created_at",
+    )
+
+    readonly_fields = ("download_count",)
+
+
+@admin.register(DownloadEvent)
+class DownloadEventAdmin(admin.ModelAdmin):
+    list_display = ("package", "timestamp")
+    list_filter = ("package",)
--- packages/models.py
+++ packages/models.py
@@ -1,3 +1,54 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+
+class Package(models.Model):
+    name = models.CharField(unique=True, max_length=32)
+    slug = models.SlugField(unique=True, editable=False)
+
+    owner = models.ForeignKey(
+        get_user_model(), related_name="packages", on_delete=models.CASCADE
+    )
+
+    description = models.TextField()
+    version = models.CharField(max_length=8)
+    author = models.CharField(max_length=16)
+    deprecated = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    readme = models.TextField()
+    archive = models.FileField(upload_to="packages")
+
+    @property
+    def download_count(self):
+        return self.downloads.count()
+
+    def record_download(self):
+        DownloadEvent.objects.create(package=self)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            base = slugify(self.name)
+            slug = base
+            n = 1
+            # Ensure uniqueness
+            while Package.objects.filter(slug=slug).exists():
+                slug = f"{base}-{n}"
+                n += 1
+            self.slug = slug
+
+        super().save(*args, **kwargs)
+
+
+class DownloadEvent(models.Model):
+    package = models.ForeignKey(
+        Package, related_name="downloads", on_delete=models.CASCADE
+    )
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-timestamp"]
